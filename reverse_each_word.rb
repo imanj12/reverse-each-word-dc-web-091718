542c4afe9c13b90dfca8ed220352c46fee0301f7
Iman Jawad <imanj12@gmail.com>
2018-09-06 16:52:32 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,8 @@
 def reverse_each_word(string)
   array = string.split(" ")
   reverse_string = ""
-  array.each do |i|
+  array.each_with_index do |i|
+    if 
     reverse = i.reverse
   end
   reverse_string
